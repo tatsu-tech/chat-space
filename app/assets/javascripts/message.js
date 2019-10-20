f3c1bcdfe7e6ed9e6343c9586d2534ae03f9e513
tatsu-tech <55968601+tatsu-tech@users.noreply.github.com>
2019-10-20 19:34:49 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,7 @@
-$(function(){ 
+$(document).on('turbolinks:load', function(){
+
   function buildHTML(message){
-    var image = message.image ? `<img src="${message.image}"` : "";
+    var image = message.image ? `<img src="${message.image}">` : "";
     var html = 
     `<div class="main__center__lists" data-message-id=${message.id}>
       <div class="main__center__lists__message-name">
@@ -42,4 +43,32 @@ $('#new_message').on('submit', function(e){
    });
    return false;
  });
+
+ var autoUpdate = function(){
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var href = 'api/messages'
+    var last_message_id = $('.main__center__lists:last').data("message-id"); 
+
+    $.ajax({
+      url: href,
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+    if (messages.length !== 0) {
+      var insertHTML = '';
+      messages.forEach(function(message){
+        insertHTML = buildHTML(message);
+        $('.main__center').append(insertHTML);
+      })
+      $('.main__center').animate({scrollTop: $('.main__center')[0].scrollHeight}, 'fast');
+    }
+    })
+    .fail(function(){
+      alert('自動更新に失敗しました');
+    });
+  }
+};
+setInterval(autoUpdate, 5000);
 });
