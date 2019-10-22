d979287382f7811c70bf6323031b18feedeacd52
tatsu-tech <55968601+tatsu-tech@users.noreply.github.com>
2019-10-22 20:35:20 +0900

    unicorn restart

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -41,7 +41,7 @@ $(document).on('turbolinks:load', function() {
 
   $('#user-search-field').on("keyup", function() {
     var input = $('#user-search-field').val()
-    
+
     $.ajax({
       type: 'GET',
       url: '/users',
